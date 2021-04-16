b0cc54f435835501902bda57b583b15c379c415e
Scott Benjamin <scottbenjamin@users.noreply.github.com>
2021-04-15 21:28:17 -0600

    Update kms.tf

--- modules/vault_cluster/kms.tf
+++ modules/vault_cluster/kms.tf
@@ -2,3 +2,8 @@ resource "aws_kms_key" "vault" {
   description             = "${random_id.environment_name.hex} - Vault Unseal Key"
   deletion_window_in_days = 10
 }
+
+resource "aws_kms_alias" "vault" {
+  name          = "alias/vault/${random_id.environment_name.hex}"
+  target_key_id = aws_kms_key.vault.key_id
+}
