9bf121bb1f2a716ecda2f103d1e29ccefecda4ca
Scott Benjamin <scottbenjamin@users.noreply.github.com>
2021-04-15 21:33:14 -0600

    Update outputs.tf

--- modules/vault_cluster/outputs.tf
+++ modules/vault_cluster/outputs.tf
@@ -6,6 +6,15 @@ output "vault_address" {
   value = aws_lb.vault.dns_name
 }
 
+output "vault_kms_key" {
+  vaule = aws_kms_key.vault.key_id
+}
+
+output "vault_kms_alias" {
+  vaule = aws_kms_key_alias.vault.name
+}
+
+
 // Can be used to add additional SG rules to Vault instances.
 output "vault_security_group" {
   value = aws_security_group.vault.id
