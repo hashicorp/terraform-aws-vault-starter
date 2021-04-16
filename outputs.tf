a0a6e1dd606cd09157867aedb331c16d0a407af2
Scott Benjamin <scottbenjamin@users.noreply.github.com>
2021-04-15 21:29:17 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "vault_address" {
 output "vault_security_group" {
   value = module.vault_cluster.vault_security_group
 }
+
+output "vault_kms_key" {
+  value = module.vault_cluster.aws_kms_key.vault
+}    
