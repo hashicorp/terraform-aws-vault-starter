8490c866be0f88625182cd86cfc825df46387de7
cmdplusr <si@cmdr.co.uk>
2021-08-19 09:30:37 +0100

    Rightmost so 3.4.0

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0.0"
+      version = "~> 3.4.0"
     }
     random = {
       source  = "hashicorp/random"
