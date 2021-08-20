2699a5024e8039a62efd47d65fe90cad6a774bc7
cmdplusr <si@cmdr.co.uk>
2021-08-20 10:45:12 +0100

    replace rightmost version syntax

--- versions.tf
+++ versions.tf
@@ -4,15 +4,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.43.0"
+      version = ">= 3.43.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 2.3.0"
+      version = ">= 2.3.0"
     }
     template = {
       source  = "hashicorp/template"
-      version = "~> 2.2.0"
+      version = ">= 2.2.0"
     }
   }
 }
