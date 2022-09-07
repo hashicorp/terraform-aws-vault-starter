b456262fce05251b714fbe7eaec28c3070ea4741
dannyibishev <dannyibishev@gmail.com>
2022-09-07 13:39:34 +0300

    security group rule description rename

--- modules/load_balancer/main.tf
+++ modules/load_balancer/main.tf
@@ -12,7 +12,7 @@ resource "aws_security_group" "vault_lb" {
 
 resource "aws_security_group_rule" "vault_lb_inbound_nodes" {
   count                    = var.lb_type == "application" ? 1 : 0
-  description              = "Allow outbound traffic from load balancer to Vault nodes on port 8200"
+  description              = "Allow inbound traffic from vault nodes on port 8200"
   security_group_id        = aws_security_group.vault_lb[0].id
   type                     = "ingress"
   from_port                = 8200
